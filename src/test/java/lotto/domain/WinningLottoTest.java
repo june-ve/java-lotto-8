9b5fd978a9168fa5512f5cff8bf22509f5ccb114
방혜준 <kkngrs2@gmail.com>
2025-11-03 16:48:55 +0900

    chore(test): WinningLottoTest 불필요한 import 문 제거

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.LongSummaryStatistics;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
